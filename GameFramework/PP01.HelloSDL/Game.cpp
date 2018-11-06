cbc6e5948e8ab31bab191f77d598065f89926a8c
창훈 박 <wawworld@imrlab.hoseo.edu>
2018-11-06 12:02:48 +0900

    PP07.TextureManager

--- GameFramework/PP01.HelloSDL/Game.cpp
+++ GameFramework/PP01.HelloSDL/Game.cpp
@@ -3,11 +3,10 @@
 #include <iostream> // checked
 
 #include "Game.h"
+#include "TextureManager.h"
 
 bool Game::init(const char* title, int xpos, int ypos,int width, int height, bool fullscreen)  // checked 
 {
-	// 각자 추가해 볼 것   ( m_bRunning 관련 코드 추가  )
-	// check :: g_pWindow, g_pRenderer => m_pWindow, m_pRenderer
 	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
 	{
 		m_pWindow = SDL_CreateWindow(title, xpos, ypos, width, height, fullscreen);
@@ -22,21 +21,21 @@ bool Game::init(const char* title, int xpos, int ypos,int width, int height, boo
 
 	m_bRunning = true;  // checked 
 
+	// checked.07
 
-	SDL_QueryTexture(m_pTexture, NULL, NULL, &m_sourceRectangle.w, &m_sourceRectangle.h);
+	//SDL_QueryTexture(m_pTexture, NULL, NULL, &m_sourceRectangle.w, &m_sourceRectangle.h);
 	//SDL_Surface* pTempSurface = IMG_Load("assets/animate.png");
-	SDL_Surface* pTempSurface = IMG_Load("assets/animate-alpha.png");
+	//SDL_Surface* pTempSurface = IMG_Load("assets/animate-alpha.png");
+	//m_pTexture = SDL_CreateTextureFromSurface(m_pRenderer,pTempSurface);
+	//SDL_FreeSurface(pTempSurface);
+	//m_sourceRectangle.w = 128;
+	//m_sourceRectangle.h = 82;
+	//m_destinationRectangle.x = m_sourceRectangle.x = 0;
+	//m_destinationRectangle.y = m_sourceRectangle.y = 0;
+	//m_destinationRectangle.w = m_sourceRectangle.w;
+	//m_destinationRectangle.h = m_sourceRectangle.h;
 
-	m_pTexture = SDL_CreateTextureFromSurface(m_pRenderer,pTempSurface);
-	SDL_FreeSurface(pTempSurface);
-
-	m_sourceRectangle.w = 128;
-	m_sourceRectangle.h = 82;
-
-	m_destinationRectangle.x = m_sourceRectangle.x = 0;
-	m_destinationRectangle.y = m_sourceRectangle.y = 0;
-	m_destinationRectangle.w = m_sourceRectangle.w;
-	m_destinationRectangle.h = m_sourceRectangle.h;
+	m_textureManager.load("assets/animate-alpha.png", "animate", m_pRenderer); // checked.07
 
 	SDL_SetRenderDrawColor(m_pRenderer, 255, 0, 0, 255);
 
@@ -45,7 +44,8 @@ bool Game::init(const char* title, int xpos, int ypos,int width, int height, boo
 
 void Game::update()
 {
-	m_sourceRectangle.x = 128 * int(((SDL_GetTicks() / 100) % 6));
+	//m_sourceRectangle.x = 128 * int(((SDL_GetTicks() / 100) % 6));
+	m_currentFrame = int(((SDL_GetTicks() / 100) % 6)); // checked.07
 }
 
 void Game::render()
@@ -53,7 +53,10 @@ void Game::render()
 	// draw color로 render 지우기
 	SDL_RenderClear(m_pRenderer);
 	// 원본 사각형과 대상 사각형의 위치와 크기에 따라 화면에 다르게 나타남…  
-	SDL_RenderCopy(m_pRenderer, m_pTexture, &m_sourceRectangle, &m_destinationRectangle);
+	//SDL_RenderCopy(m_pRenderer, m_pTexture, &m_sourceRectangle, &m_destinationRectangle);
+	m_textureManager.draw("animate", 0, 0, 128, 82, m_pRenderer); // checked.07
+	m_textureManager.drawFrame("animate", 100, 100, 128, 82, 1, m_currentFrame, m_pRenderer); // checked.07
+
 	// 화면 제시하기
 	SDL_RenderPresent(m_pRenderer); 
 }
--- GameFramework/PP01.HelloSDL/Game.h
+++ GameFramework/PP01.HelloSDL/Game.h
@@ -1,6 +1,6 @@
 #pragma once
 #include "SDL.h"
-
+#include "TextureManager.h" // checked
 class Game
 {
 public:
@@ -16,9 +16,14 @@ public:
 private:
 	SDL_Window * m_pWindow;
 	SDL_Renderer* m_pRenderer;
-	SDL_Texture* m_pTexture; // the new SDL_Texture variable
-	SDL_Rect m_sourceRectangle; // 원본 사각형 
-	SDL_Rect m_destinationRectangle; // 대상 사각형 
+
+	// checked.07
+	//SDL_Texture* m_pTexture; // the new SDL_Texture variable
+	//SDL_Rect m_sourceRectangle; // 원본 사각형 
+	//SDL_Rect m_destinationRectangle; // 대상 사각형 
+
+	TextureManager m_textureManager; // checked.07
+	int m_currentFrame; // checked.07
 
 	bool m_bRunning;
 };
